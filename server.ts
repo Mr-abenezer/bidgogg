f2623a04e2b73fa4278ec0df6bb30ec1bb8863a0
Mr-abenezer <abenu7010@gmail.com>
2026-08-14 17:41:21 +0300

    feat: allow admin key headers in CORS policy
    Update Access-Control-Allow-Headers to include X-Admin-Key and admin-key for administrative API requests.

--- server.ts
+++ server.ts
@@ -25,7 +25,7 @@ async function startServer() {
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Telegram-Init-Data, Accept');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Telegram-Init-Data, X-Admin-Key, admin-key, Accept');
     if (req.method === 'OPTIONS') {
       return res.sendStatus(204);
     }
